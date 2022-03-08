f6e325b80cee64f7293b5dc031f8fdbb6610056a
Lori McCurry <lori.mccurry@gmail.com>
2022-03-08 14:43:00 -0600

    player ready actions wired up and referee assigned

--- server.js
+++ server.js
@@ -12,6 +12,19 @@ server.listen(PORT);
 
 console.log(`Listening on port ${PORT}...`);
 
+let readyPlayerCount = 0;
+
 io.on('connection', (socket) => {
   console.log('a user connected', socket.id);
+
+  socket.on('ready', () => {
+    console.log('Player ready', socket.id);
+    readyPlayerCount++;
+
+    // 2nd player will always be the referee
+    // we emit that to all players
+    if (readyPlayerCount === 2) {
+      io.emit('startGame', socket.id);
+    }
+  });
 });
